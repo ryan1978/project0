efae8f32539d7fa360f933eaaa09f443cfa4d808
Ryan Gilreath <ryan.gilreath@monstermedia.net>
2015-07-22 20:32:00 -0400

    Initial commit

---
+++ app/src/main/java/com/example/jagr/project0/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.jagr.project0;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        Button button2 = (Button) findViewById(R.id.button2);
+        Button button3 = (Button) findViewById(R.id.button3);
+        Button button4 = (Button) findViewById(R.id.button4);
+        Button button5 = (Button) findViewById(R.id.button5);
+        Button button6 = (Button) findViewById(R.id.button6);
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        String appName = null;
+
+        switch (view.getId()) {
+            case R.id.button1: appName = getString(R.string.button1); break;
+            case R.id.button2: appName = getString(R.string.button2); break;
+            case R.id.button3: appName = getString(R.string.button3); break;
+            case R.id.button4: appName = getString(R.string.button4); break;
+            case R.id.button5: appName = getString(R.string.button5); break;
+            case R.id.button6: appName = getString(R.string.button6); break;
+        }
+
+        if (appName != null) {
+            Toast.makeText(this, "This button will launch " + appName, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
